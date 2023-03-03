0247fd609fa03ed7308f22aea8503ae1184f0dcb
kaleb birhanu <birhanukaleb62@gmail.com>
2023-03-03 15:30:56 +0300

    loop

---
+++ mod4_solution/js/index.js
@@ -0,0 +1,10 @@
+// print Goodbye + name for names start j lettercd .. and  Hello + name for  names start other letters
+    var names = ["william", "jhon", "eric", "angela", "json"];
+    for (var i = 0; i<names.length; i++)
+        if (names[i][0] == "j")
+         console.log("Goodbye " + names[i]);
+        else {
+            console.log("Hello " + names[i]);
+        }
+    
+
